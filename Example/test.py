c5caf16fda6cd2c141f9632c67fab1aee32daa97
Amid <amid.nayerhoda@gmail.com>
2025-05-05 11:25:16 +0200

    ver 0.0.3 uploaded

---
+++ Example/test.py
@@ -0,0 +1,3 @@
+from JTBrix import run_test
+result = run_test("../src/JTBrix/data/config.yml", "../src/JTBrix/data/static/") 
+
--- src/JTBrix/__init__.py
+++ src/JTBrix/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Amid Nayerhoda"""
 __email__ = 'Nayerhoda@infn.it'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 
 # Import core functionality to the top level
@@ -11,7 +11,7 @@ from JTBrix.questionnaire import screens   # Example: expose a core class
 from JTBrix.utils import port  # Example: utility functions
 from JTBrix.ui import main  # Example: if you have a UI component
 from JTBrix.screen_config import flow_config  # Example: if you have a configuration module
-
+from JTBrix.experiment.run_experiment import run_test
 
 __all__ = [
     "run_session",
@@ -19,4 +19,5 @@ __all__ = [
     "port",
     "main",
     "flow_config",
+    "run_test",
 ]
