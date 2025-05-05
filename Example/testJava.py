60f3389322e865a7f7f33ccb1c8d512f3cfdc26f
Amid <amid.nayerhoda@gmail.com>
2025-05-05 02:33:04 +0200

    -

--- Example/testJava.py
+++ Example/testJava.py
@@ -3,20 +3,21 @@ from JTBrix.experiment.run_session import run_entire_test_config
 from JTBrix.screen_config import flow_config
 
 
+config_path = ""
+static_folder="/Users/amid/GitHub/JTBrix/Example/static/"
 from JTBrix.utils.config import read_experiment_config
-config, order = read_experiment_config("config.yml")
+config, order = read_experiment_config(config_path)
 print("CONFIG:")
-for item in config:
-    print(item)
 
-#print("\nSELECTED ORDER:", order)
+
+print("\nSELECTED ORDER:", order)
    
-if __name__ == "__main__":
 
 
-    flow_config.clear()
-    flow_config.extend(config)
-    results = run_entire_test_config(config, static_folder="/Users/amid/GitHub/JTBrix/Example/static/")
-    print(results)
+
+flow_config.clear()
+flow_config.extend(config)
+results = run_entire_test_config(config, static_folder=static_folder)
+print(results)
 
 
