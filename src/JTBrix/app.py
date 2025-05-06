5d22b9cb2e8cc36e762c4622587955a240d27802
Amid <amid.nayerhoda@gmail.com>
2025-05-07 00:23:39 +0200

    -

--- src/JTBrix/app.py
+++ src/JTBrix/app.py
@@ -12,8 +12,13 @@ import JTBrix
 
 # Setup paths
 jtbrix_root = Path(JTBrix.__file__).parent
-template_path = jtbrix_root / "templates"
-static_path = jtbrix_root / "data" / "static"
+
+template_path = "/content/JTBrix/src/templates/" 
+config_path =  "/content/JTBrix/src/data/config.yml"
+static_path = "/content/JTBrix/src/data/static/"
+
+results_path = "/content/JTBrix/src/data/results/"
+
 
 # Create the app
 app = Flask(__name__, static_folder=static_path, template_folder=template_path)
@@ -23,7 +28,7 @@ app.register_blueprint(screens)
 # ✅ Add /run_experiment route
 @app.route("/run_experiment")
 def run_experiment():
-    results, order = run_test("data/config.yml", "data/static/", timeout=300)
-    structured_output = build_full_structured_result(results, "data/config.yml", execution_order=order)
-    save_structured_output(structured_output, save_path="data/results/", name="Test_data")
+    results, order = run_test(config_path, static_path , timeout=300)
+    structured_output = build_full_structured_result(results, config_path, execution_order=order)
+    save_structured_output(structured_output, save_path=results_path, name="Test_data")
     return jsonify({"status": "success", "order": order, "summary": structured_output})
