f61308262d7358f1b1c346dbc4abc1a96d5a3c60
Amid <amid.nayerhoda@gmail.com>
2025-05-08 19:17:27 +0200

    Update run_experiment.py

--- src/JTBrix/experiment/run_experiment.py
+++ src/JTBrix/experiment/run_experiment.py
@@ -56,8 +56,7 @@ def run_test_local(app, config, order, timeout= 600 ) :
 
 
 def run_test_colab(app, config, order, timeout= 600 ):
-    def run_app():
-        app.run(port=port, debug=False, use_reloader=False)
+    run_app=  app.run(port=port, debug=False, use_reloader=False)
 
     port = find_free_port()
     print ("Running on Google Colab")
